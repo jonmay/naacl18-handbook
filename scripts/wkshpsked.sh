771d3990e5f6def8ba1d79fc4f8e0fd3840d755d
Jon May <github@jonmay.net>
2018-04-30 18:14:29 -0700

    convenience scripts

---
+++ scripts/wkshpsked.sh
@@ -0,0 +1,4 @@
+for dir in $(ls data); do 
+   [[ ! -d "auto/$dir" ]] && mkdir auto/$dir
+   cat data/$dir/proceedings/order | ./scripts/order2schedule_workshop.pl $dir > auto/$dir/schedule.tex
+ done
